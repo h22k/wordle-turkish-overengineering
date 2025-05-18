f662b983fbe76f8b0af9597d93f8d583e462a77b
Melike Sinem Kaya <msinemkaya6@gmail.com>
2025-05-18 19:51:57 +0300

    fix: fixed mobile

--- client/src/gameConfig.ts
+++ client/src/gameConfig.ts
@@ -3,9 +3,9 @@ import { LetterStatus } from './types/game'
 export const WORD_LENGTH: number = +import.meta.env.VITE_WORD_LENGTH
 export const MAX_ATTEMPTS: number = +import.meta.env.VITE_MAX_ATTEMPTS
 export const KEYBOARD_LAYOUT: string[][] = [
-  [ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Ğ', 'Ü' ],
+  [ 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Ğ', 'Ü' ],
   [ 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ş', 'İ' ],
-  [ 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Ö', 'Ç', 'BACKSPACE' ],
+  [ 'ENTER', 'Z', 'C', 'V', 'B', 'N', 'M', 'Ö', 'Ç', 'BACKSPACE' ],
 ]
 export const STATUS_COLOR: Record<LetterStatus, string> = {
   [LetterStatus.CORRECT]: 'bg-correct',
@@ -13,4 +13,4 @@ export const STATUS_COLOR: Record<LetterStatus, string> = {
   [LetterStatus.ABSENT]: 'bg-absent',
   [LetterStatus.EMPTY]: 'bg-transparent border-2 border-absent',
 }
-export const VALID_LETTERS_REGEX = /^[A-Za-zĞğÜüŞşİıÇçÖö]$/
+export const VALID_LETTERS_REGEX = /^[ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz]$/
