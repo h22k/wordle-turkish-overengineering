59d2a042fb9540c8dc3a439ef11ce68088536956
Melike Sinem Kaya <msinemkaya6@gmail.com>
2025-05-19 00:18:27 +0300

    fix: fixed mobile, started sending request

--- client/src/hooks/useKeyboardEvents.ts
+++ client/src/hooks/useKeyboardEvents.ts
@@ -1,27 +1,19 @@
 import { useKeyboard } from './useKeyboard'
-import React, { useState } from 'react'
+import React from 'react'
 import { VALID_LETTERS_REGEX } from '../gameConfig'
 
 export const useKeyboardEvents = (inputRef?: React.RefObject<HTMLInputElement | null>) => {
   const { handleChange } = useKeyboard()
 
   const processKey = (key: string) => {
-    const upperKey = key.toUpperCase()
-    if ( VALID_LETTERS_REGEX.test(upperKey) ) {
+    const upperKey = key.toLocaleUpperCase('tr')
+    if (VALID_LETTERS_REGEX.test(upperKey)) {
       handleChange(upperKey)
-      setTimeout(() => {
-        ( inputRef?.current?.nextElementSibling as HTMLInputElement | null )?.focus()
-      }, 0)
-    }
-
-    else if ( upperKey === 'BACKSPACE' ) {
+      ;(inputRef?.current?.nextElementSibling as HTMLInputElement | null)?.focus()
+    } else if (upperKey === 'BACKSPACE') {
       handleChange('BACKSPACE')
-      setTimeout(() => {
-        ( inputRef?.current?.previousElementSibling as HTMLInputElement | null )?.focus()
-      }, 0)
-    }
-
-    else if ( upperKey === 'ENTER' ) {
+      ;(inputRef?.current?.previousElementSibling as HTMLInputElement | null)?.focus()
+    } else if (upperKey === 'ENTER') {
       handleChange('ENTER')
     }
   }
